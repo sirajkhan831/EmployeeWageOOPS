02a769a73b15ecdb73c3ced95664c8b77db4a0db
Siraj <sirajkhanc2305@gmail.com>
2021-08-26 13:43:28 +0530

    WorkingDays

--- src/com/bridgelabz/EmployeeWageOOPS.java
+++ src/com/bridgelabz/EmployeeWageOOPS.java
@@ -1,34 +1,37 @@
 package com.bridgelabz;
 
 public class EmployeeWageOOPS {
-    public static final int IS_FULL_TIME=2;
-    public static final  int IS_PART_TIME =1;
-    public static final  int EMP_RATE_PER_HOUR =20;
+
+    public static final int IS_FULL_TIME = 2;
+    public static final int IS_PART_TIME = 1;
+    public static final int EMP_RATE_PER_HOUR = 8;
+    public static final int Working_Days = 20;
+
 
     public static void main(String[] args) {
-        int empCheck =(int) Math.floor(Math.random()*10)%3;
-        EmployeeWageOOPS obj=new EmployeeWageOOPS();
-        obj.wageCalc(empCheck);
+        int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+        EmployeeWageOOPS obj = new EmployeeWageOOPS();
+        obj.workingDays(empCheck);
     }
 
-
-    public void wageCalc(int empCheck) {
-        int wage=0;
-        int empHours=0;
+    public void workingDays(int empCheck) {
+        int wage = 0;
+        int empHours = 0;
         switch (empCheck) {
             case IS_FULL_TIME:
-                empHours=8;
+                empHours = 8;
                 break;
 
             case IS_PART_TIME:
-                empHours=4;
+                empHours = 4;
                 break;
 
             default:
-                empHours=0;
+                empHours = 0;
         }
-        wage=empHours*EMP_RATE_PER_HOUR;
-        System.out.println("Emp wage :" +wage);
-
+        wage = empHours * EMP_RATE_PER_HOUR * Working_Days;
+        System.out.println("Emp wage :" + wage);
     }
 }
+
+
