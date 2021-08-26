90da964a422418939a9fd3ec1a15b6b15160fe2a
Siraj <sirajkhanc2305@gmail.com>
2021-08-26 13:37:04 +0530

    DailyWage

--- src/com/bridgelabz/EmployeeWageOOPS.java
+++ src/com/bridgelabz/EmployeeWageOOPS.java
@@ -1,20 +1,31 @@
 package com.bridgelabz;
 
-public class EmployeeWageOOPS {//checking method
+public class EmployeeWageOOPS {
 
-    public void empPresentCheck() {
+    //checking method
+    public double empPresentCheck() {
         double empCheck = Math.floor(Math.random() * 10) % 2;
         if (empCheck == 0) {
             System.out.println("Employee Present");
         } else {
             System.out.println("Employee absent");
         }
+        return empCheck;
+    }
+
+    public void dailyWageEmployee(double check) {
+        int wage = 20;
+        int hour = 8;
+        if (check == 0) {
+            int dailyWage = wage * hour;
+            System.out.println("Daily wage : " + dailyWage);
+        }
     }
 
     //main method
     public static void main(String[] args) {
         EmployeeWageOOPS obj = new EmployeeWageOOPS();
-        obj.empPresentCheck();
-
+        double check = obj.empPresentCheck();
+        obj.dailyWageEmployee(0);
     }
 }
